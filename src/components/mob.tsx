39a154afac3dbc0cf457964a99d46361dd0fa417
ebuinevicius <embuinevicius@gmail.com>
2020-11-08 15:59:44 -0600

    Added new styled-component HPStyles
    Developing this helped EB understand more on how styled components work with JS.

--- src/components/mob.tsx
+++ src/components/mob.tsx
@@ -6,9 +6,17 @@ const MobStyles = styled.div`
   font-size: 1.5rem;
 `;
 
+const HPStyles = styled.span`
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #023843;
+  margin-right: 0.75rem;
+`;
+
 const MobName = styled.span`
   font-weight: bold;
   margin-right: 1rem;
+  font-style: italic;
 `;
 
 interface HealthProps {
@@ -16,6 +24,7 @@ interface HealthProps {
   maxHP: number;
 }
 
+// putting <HealthProps> next to span tells you the type of variable that props is
 const MobHealth = styled.span<HealthProps>`
   color: ${(props) => (props.currentHP / props.maxHP >= 0.5 ? 'green' : 'red')};
 `;
@@ -28,7 +37,7 @@ export const Mob: FunctionComponent<MobProps> = ({ mob }) => {
   return (
     <MobStyles>
       <MobName>{`${mob.name}`}</MobName>
-      <span>HP:</span>
+      <HPStyles>HP:</HPStyles>
       <MobHealth currentHP={mob.currentHP} maxHP={mob.maxHP}>
         {`${mob.currentHP}/${mob.maxHP} `}
       </MobHealth>
