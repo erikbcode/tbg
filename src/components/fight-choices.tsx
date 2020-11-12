11ddb1d3a5a37c6c01460d97a8a56041fc91855e
ebuinevicius <embuinevicius@gmail.com>
2020-11-11 18:22:55 -0600

    Modifications to fight-choices
    Changed buttons so that 'Use Potion' is only usable when the user has taken damage and stat message is not showing.

--- src/components/fight-choices.tsx
+++ src/components/fight-choices.tsx
@@ -2,24 +2,26 @@ import React, { FunctionComponent } from 'react';
 
 export interface FightChoicesProps {
   disabled?: boolean; // ? tells you this var is optional
+  potionDisabled?: boolean;
   onAttack: () => void;
   onPotion: () => void;
   onRun: () => void;
 }
 
-export const FightChoices: FunctionComponent<FightChoicesProps> = ({
-  disabled,
-  onAttack,
-  onPotion,
-  onRun,
-}) => {
+export const FightChoices: FunctionComponent<FightChoicesProps> = (props) => {
+  const { disabled, potionDisabled, onAttack, onPotion, onRun } = props;
+
   // does the above line make FightChoices into its own function? Or why is it an arrow function?
   return (
     <div>
       <button disabled={disabled} type="button" onClick={onAttack}>
         Attack
       </button>
-      <button disabled={disabled} type="button" onClick={onPotion}>
+      <button
+        disabled={potionDisabled || disabled}
+        type="button"
+        onClick={onPotion}
+      >
         Use Potion
       </button>
       <button disabled={disabled} type="button" onClick={onRun}>
@@ -31,4 +33,31 @@ export const FightChoices: FunctionComponent<FightChoicesProps> = ({
 
 FightChoices.defaultProps = {
   disabled: false,
+  potionDisabled: false,
 };
+
+// Below are various ways of writing the same function
+
+interface AddProps {
+  a: number;
+  b: number;
+}
+
+export function add(props: AddProps): number {
+  return props.a + props.b;
+}
+
+export function addb(props: AddProps): number {
+  const { a, b } = props;
+  return a + b;
+}
+
+export function addc({ a, b }: AddProps): number {
+  return a + b;
+}
+
+export const add2 = (a: number, b: number): number => {
+  return a + b;
+};
+
+export const add3 = (a: number, b: number): number => a + b;
